eff059e30ba3cd6067f9e4b6435a591477aa0491
Prajwal R <ruyketsu.prajwal@gmail.com>
2025-07-09 10:42:13 +0530

    Updated File structure

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,30 +1,30 @@
 package config
 
 import (
-    "os"
+	"os"
 )
 
 type Config struct {
-    ServerAddress string
-    MediaDir      string
-    StaticDir     string
-    DefaultRoom   string
-    DefaultFilePath string
+	ServerAddress   string
+	MediaDir        string
+	StaticDir       string
+	DefaultRoom     string
+	DefaultFilePath string
 }
 
 func Load() *Config {
-    return &Config{
-        ServerAddress: getEnv("SERVER_ADDRESS", "0.0.0.0:8000"),
-        MediaDir:      getEnv("MEDIA_DIR", "media"),
-        StaticDir:     getEnv("STATIC_DIR", "static"),
-        DefaultRoom:   getEnv("DEFAULT_ROOM", "default"),
-        DefaultFilePath: getEnv("filePath", "cpp_src/x64/Debug/VRenv(raylib).exe"),
-    }
+	return &Config{
+		ServerAddress:   getEnv("SERVER_ADDRESS", "0.0.0.0:8000"),
+		MediaDir:        getEnv("MEDIA_DIR", "media"),
+		StaticDir:       getEnv("STATIC_DIR", "static"),
+		DefaultRoom:     getEnv("DEFAULT_ROOM", "default"),
+		DefaultFilePath: getEnv("filePath", "execs/VRenv(raylib).exe"),
+	}
 }
 
 func getEnv(key, defaultValue string) string {
-    if value := os.Getenv(key); value != "" {
-        return value
-    }
-    return defaultValue
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
